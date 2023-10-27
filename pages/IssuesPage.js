2fad688d4dad8f5c016792026962dbc5e2ed827b
wasadar <wasadar2@gmail.com>
2023-10-27 15:52:48 +0300

    change certificates setting

--- pages/IssuesPage.js
+++ pages/IssuesPage.js
@@ -19,6 +19,6 @@ class IssuesPage {
     }
 
     async gotoIssuesPage() {
-        await this.page.goto("https://www.redmine.org/projects/redmine/issues");
+        await this.page.goto("https://www.redmine.org/projects/redmine/issues", { ignoreHTTPSErrors: true });
     }
 }
